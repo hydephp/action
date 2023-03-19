89c71ab2a62b5184842b2ce12f4cf6813e2e18f5
Caen De Silva <caen@desilva.se>
2023-03-19 19:53:22 +0100

    Add color option

--- tests/lint.php
+++ tests/lint.php
@@ -2,7 +2,11 @@
 
 declare(strict_types=1);
 
-function write(string $line): void
+function write(string $line, string $color = null): void
 {
+    if ($color) {
+        $line = "\033[{$color}m$line\033[0m";
+    }
+
     echo $line . PHP_EOL;
 }
