d07bdfd93467a0e21ca25aba5ccd05cbd87624c3
Caen De Silva <caen@desilva.se>
2023-03-20 18:51:55 +0100

    Implement the verification script

--- tests/github-action-test-project-1/verify.php
+++ tests/github-action-test-project-1/verify.php
@@ -1,1 +1,8 @@
 <?php
+
+assert(is_dir('build'));
+assert(is_file('build/index.html'));
+assert(is_file('build/404.html'));
+assert(is_file('build/sitemap.xml'));
+assert(is_dir('build/media'));
+assert(is_file('build/media/app.css'));
