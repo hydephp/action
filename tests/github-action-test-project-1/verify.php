91008a6aefb43cc6af53c457f301486df6d03e4f
Caen De Silva <caen@desilva.se>
2023-03-20 20:58:36 +0100

    Update script to support closures

--- tests/github-action-test-project-1/verify.php
+++ tests/github-action-test-project-1/verify.php
@@ -3,13 +3,17 @@
 $contents = file(__FILE__);
 $exitCode = 0;
 
-function verify(bool $result): void
+function verify(bool|Closure $result): void
 {
     global $contents;
     global $exitCode;
 
     $line = trim($contents[debug_backtrace()[0]['line'] - 1]);
     $testName = substr($line, 7, strpos($line, ');') - 7);
+
+    if ($result instanceof Closure) {
+        $result = $result();
+    }
 
     if ($result) {
         echo 'passed';
