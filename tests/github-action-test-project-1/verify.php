55ac6d8a4a4e01085438fd5ccf0621f0b769f339
Caen De Silva <caen@desilva.se>
2023-03-20 20:28:00 +0100

    Debug

--- tests/github-action-test-project-1/verify.php
+++ tests/github-action-test-project-1/verify.php
@@ -1,5 +1,5 @@
 <?php
-
+echo "debug";
 echo assert(is_dir('debug')) ? 'passed' : 'failed';
 echo assert(is_file('build/index.html')) ? 'passed' : 'failed';
 echo assert(is_file('build/404.html')) ? 'passed' : 'failed';
