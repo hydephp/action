480fd59f4262aca7a54611ab28d07c1b2e6dcfab
Caen De Silva <caen@desilva.se>
2023-03-20 20:39:16 +0100

    Create nicer testing API

--- tests/github-action-test-project-1/verify.php
+++ tests/github-action-test-project-1/verify.php
@@ -1,10 +1,39 @@
 <?php
-echo "debug";
-echo assert(is_dir('debug')) ? 'passed' : 'failed';
-echo assert(is_file('build/index.html')) ? 'passed' : 'failed';
-echo assert(is_file('build/404.html')) ? 'passed' : 'failed';
-echo assert(is_file('build/sitemap.xml')) ? 'passed' : 'failed';
-echo assert(is_dir('build/media')) ? 'passed' : 'failed';
-echo assert(is_file('build/media/app.css')) ? 'passed' : 'failed';
 
-echo assert(false) ? 'passed' : 'failed'; // Test it fails build
+$contents = file(__FILE__);
+$exitCode = 0;
+
+function verify(bool $result): void
+{
+    global $contents;
+
+    // get line calling this function
+    $trace = debug_backtrace();
+    $line = $trace[0]['line'];
+    // extract test name from line
+    $testName = trim($contents[$line - 1]);
+    // get string between first and second quote
+    // $testName = preg_replace('/^.*?\'(.*?)\'.*?$/', '$1', $testName) ?? $testName;
+    // get string inside verify( and last );
+    $testName = substr($testName, 7, strpos($testName, ');') - 7);
+
+    if ($result) {
+        echo 'passed: ';
+    } else {
+        echo 'failed: ';
+        global $exitCode;
+        $exitCode = 1;
+    }
+    echo $testName . PHP_EOL;
+}
+verify(is_dir('build'));
+
+verify(is_file('build/index.html'));
+verify(is_file('build/404.html'));
+verify(is_file('build/sitemap.xml'));
+verify(is_dir('build/media'));
+verify(is_file('build/media/app.css'));
+
+verify(false); // Test it fails build
+
+exit($exitCode);
