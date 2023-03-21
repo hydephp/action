f0f1d720fbb496e36ec4bed1ed0dfa7f87cd086a
Caen De Silva <caen@desilva.se>
2023-03-21 11:10:57 +0100

    Formatting

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,7 +16,7 @@ final class TestCase
 
     public static function boot(string $file): void
     {
-        echo "Running tests for " . basename(dirname($file)) . "\n\n";
+        echo 'Running tests for ' . basename(dirname($file)) . "\n\n";
 
         self::$instance = new self($file);
     }
