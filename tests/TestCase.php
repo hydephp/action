5e57bc0fbf676795adce8ad2667b90e67fd8a2ca
Caen De Silva <caen@desilva.se>
2023-03-21 11:16:45 +0100

    Move up interface

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,18 @@
 
 declare(strict_types=1);
 
+interface ANSI {
+    const BLACK = "\033[30m";
+    const RED = "\033[31m";
+    const GREEN = "\033[32m";
+    const YELLOW = "\033[33m";
+    const BLUE = "\033[34m";
+    const MAGENTA = "\033[35m";
+    const CYAN = "\033[36m";
+    const WHITE = "\033[37m";
+    const RESET = "\033[0m";
+}
+
 final class TestCase
 {
     private const PASSED = ANSI::GREEN.'passed'.ANSI::RESET;
@@ -49,18 +61,6 @@ final class TestCase
 
         echo ($result ? self::PASSED : self::FAILED) . ": $testName\n";
     }
-}
-
-interface ANSI {
-    const BLACK = "\033[30m";
-    const RED = "\033[31m";
-    const GREEN = "\033[32m";
-    const YELLOW = "\033[33m";
-    const BLUE = "\033[34m";
-    const MAGENTA = "\033[35m";
-    const CYAN = "\033[36m";
-    const WHITE = "\033[37m";
-    const RESET = "\033[0m";
 }
 
 function verify(bool|Closure $result): void
