51d1e4b3b77558271f56260ef9e85c03cde6a902
Caen De Silva <caen@desilva.se>
2023-03-22 22:27:38 +0100

    Add todo

--- src/validate-input.sh
+++ src/validate-input.sh
@@ -7,3 +7,5 @@ if [[ "$value" != "artifact" ]] && [[ "$value" != "pages" ]]; then
   echo "Invalid input for deploy-to: $value"
   exit 1
 fi
+
+# Todo: Add warning if version input is set but install strategy is set to anonymous project
