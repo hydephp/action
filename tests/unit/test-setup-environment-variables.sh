3012784bb6baa0f483b51e751b09a14533c3e26b
Caen De Silva <caen@desilva.se>
2023-03-22 22:47:24 +0100

    Create test-setup-environment-variables.sh

---
+++ tests/unit/test-setup-environment-variables.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+test_copy_env_example() {
+  base_dir="$(pwd)"
+
+  # Create a temporary directory and navigate to it
+  temp_dir=$(mktemp -d)
+  cd "$temp_dir" || exit
+
+  # Create an example file
+  echo "foo" > .env.example
+
+  # Call the script
+  "$base_dir"/src/setup-environment-variables.sh
+
+  # Assert that the .env file was created
+  if [[ ! -f .env ]]; then
+    echo "Test failed: .env file not created"
+    exit 1
+  fi
+
+  # Assert that the .env file is identical to the .env.example file
+  if ! cmp -s .env .env.example; then
+    echo "Test failed: .env file not identical to .env.example"
+    exit 1
+  fi
+
+  # Clean up the temporary directory
+  rm -r "$temp_dir"
+}
+
+# Run the test
+test_copy_env_example
+
+# Clean up
+cd "$base_dir" || exit
+
+# Exit with success code
+echo "Test passed"
+exit 0
