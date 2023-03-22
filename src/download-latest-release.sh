52aacea4abc77fb1696b67caa269ead7e9238b53
Caen De Silva <caen@desilva.se>
2023-03-22 22:18:21 +0100

    Extract logic to shell script

--- src/download-latest-release.sh
+++ src/download-latest-release.sh
@@ -1,2 +1,18 @@
 # Runs if: steps.determine-install-strategy.outputs.install-strategy == 'archive'
 # With input: framework-version
+
+# Copy all root files to backup directory
+cp -r . ../backup
+# Download latest version of HydePHP
+git clone https://github.com/hydephp/hyde.git ../hyde --depth 1
+# Copy HydePHP files to root
+cp -r ../hyde/* .
+# Remove default pages
+rm _pages/index.blade.php _pages/404.blade.php
+# Copy Backup files to root
+cp -r ../backup/* .
+
+# If version is set to something other than latest, require that framework version
+if [[ "${{ inputs.framework-version }}" != "latest" ]]; then
+composer require hyde/framework:${{ inputs.framework-version }} --no-install
+fi
