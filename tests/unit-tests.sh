88e7e4e47bdbc430e8a832b2a773790805e92a3a
Caen De Silva <caen@desilva.se>
2023-03-22 23:01:35 +0100

    Buffer exit codes to fail CI if unit test fails

--- tests/unit-tests.sh
+++ tests/unit-tests.sh
@@ -3,11 +3,19 @@ echo "Running unit tests"
 # Track time in milliseconds
 start=$(date +%s%3N)
 
+exit_code=0
+
 for test in tests/unit/*.sh; do
   echo "Running $test"
-  bash "$test"
+  if ! bash "$test"; then
+    echo "Test failed: $test"
+    exit_code=1
+  fi
 done
 
 # Display time in milliseconds
 end=$(date +%s%3N)
 echo "Time: $((end - start))ms"
+
+echo "Unit tests complete. Exiting with code $exit_code"
+exit "$exit_code"
