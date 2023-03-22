094021f47e3a93f490404104666e5c549532f782
Caen De Silva <caen@desilva.se>
2023-03-22 22:48:30 +0100

    Track and display time in milliseconds

--- tests/unit-tests.sh
+++ tests/unit-tests.sh
@@ -1,6 +1,13 @@
 echo "Running unit tests"
 
+# Track time in milliseconds
+start=$(date +%s%3N)
+
 for test in tests/unit/*.sh; do
   echo "Running $test"
   bash "$test"
 done
+
+# Display time in milliseconds
+end=$(date +%s%3N)
+echo "Time: $((end - start))ms"
