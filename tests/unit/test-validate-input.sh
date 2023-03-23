60fbfdec55ef69da19329fcceb202569bb1af6c9
Caen De Silva <caen@desilva.se>
2023-03-23 10:33:15 +0100

    Test all branches

--- tests/unit/test-validate-input.sh
+++ tests/unit/test-validate-input.sh
@@ -9,9 +9,16 @@ if [[ $output != "" ]]; then
   exit 1
 fi
 
-# Test 2: Invalid input - should fail
+# Test 2: Valid input - should pass
+output=$(bash "$base_dir/validate-input.sh" pages)
+if [[ $output != "" ]]; then
+  echo "Test 2 Failed"
+  exit 1
+fi
+
+# Test 3: Invalid input - should fail
 output=$(bash "$base_dir/validate-input.sh" invalid-input)
 if [[ $output != "Invalid input for deploy-to: invalid-input" ]]; then
-  echo "Test 2 Failed"
+  echo "Test 3 Failed"
   exit 1
 fi
