6756f74989e097fc0132691ff9e836a3b5d27542
Caen De Silva <caen@desilva.se>
2023-03-23 10:32:30 +0100

    Regenerate simpler test

--- tests/unit/test-validate-input.sh
+++ tests/unit/test-validate-input.sh
@@ -1,49 +1,18 @@
 #!/bin/bash
 
-# Test case with valid input for deploy-to
-test_valid_input() {
-  base_dir="$(pwd)"
+# Test 1: Valid input - should pass
+output=$(bash deploy.sh artifact)
+if [[ $output != "" ]]; then
+  echo "Test 1 Failed"
+  exit 1
+fi
 
-  GITHUB_OUTPUT="$base_dir/output.txt"
-  touch "$GITHUB_OUTPUT"
-  export GITHUB_OUTPUT;
+# Test 2: Invalid input - should fail
+output=$(bash deploy.sh invalid-input)
+if [[ $output != "Invalid input for deploy-to: invalid-input" ]]; then
+  echo "Test 2 Failed"
+  exit 1
+fi
 
-  value="pages"
-  chmod +x "$base_dir/validate-input.sh"
-  "$base_dir/validate-input.sh" "$value"
-
-  # Assert that nothing is written to the output file, indicating success
-  assertTrue "Expected no output to be written to $GITHUB_OUTPUT" "[ ! -s $GITHUB_OUTPUT ]"
-}
-
-# Test case with invalid input for deploy-to
-test_invalid_input() {
-  base_dir="$(pwd)"
-
-  GITHUB_OUTPUT="$base_dir/output.txt"
-  touch "$GITHUB_OUTPUT"
-  export GITHUB_OUTPUT;
-
-  value="invalid"
-  chmod +x "$base_dir/validate-input.sh"
-  "$base_dir/validate-input.sh" "$value"
-
-  # Assert that the error message is written to the output file, indicating failure
-  grep -q "Invalid input for deploy-to: $value" "$GITHUB_OUTPUT"
-  assertTrue "Expected 'Invalid input for deploy-to: $value' to be written to $GITHUB_OUTPUT" $?
-}
-
-# Define assertTrue function
-assertTrue() {
-  if [ "$2" -ne 0 ]; then
-    echo "$1"
-    exit 1
-  fi
-}
-
-# Run the tests
-test_valid_input
-test_invalid_input
-
-# Clean up
-rm -f output.txt
+echo "All tests passed!"
+exit 0
