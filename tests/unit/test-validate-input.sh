240cd3a7b3e90b54aa2b84d8a607595c271a38f6
Caen De Silva <caen@desilva.se>
2023-03-23 10:30:28 +0100

    Fix up test

--- tests/unit/test-validate-input.sh
+++ tests/unit/test-validate-input.sh
@@ -9,8 +9,8 @@ test_valid_input() {
   export GITHUB_OUTPUT;
 
   value="pages"
-  chmod +x "$base_dir/deploy.sh"
-  "$base_dir/deploy.sh" "$value"
+  chmod +x "$base_dir/validate-input.sh"
+  "$base_dir/validate-input.sh" "$value"
 
   # Assert that nothing is written to the output file, indicating success
   assertTrue "Expected no output to be written to $GITHUB_OUTPUT" "[ ! -s $GITHUB_OUTPUT ]"
@@ -25,10 +25,25 @@ test_invalid_input() {
   export GITHUB_OUTPUT;
 
   value="invalid"
-  chmod +x "$base_dir/deploy.sh"
-  "$base_dir/deploy.sh" "$value"
+  chmod +x "$base_dir/validate-input.sh"
+  "$base_dir/validate-input.sh" "$value"
 
   # Assert that the error message is written to the output file, indicating failure
   grep -q "Invalid input for deploy-to: $value" "$GITHUB_OUTPUT"
   assertTrue "Expected 'Invalid input for deploy-to: $value' to be written to $GITHUB_OUTPUT" $?
 }
+
+# Define assertTrue function
+assertTrue() {
+  if [ "$2" -ne 0 ]; then
+    echo "$1"
+    exit 1
+  fi
+}
+
+# Run the tests
+test_valid_input
+test_invalid_input
+
+# Clean up
+rm -f output.txt
