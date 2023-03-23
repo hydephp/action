56edaf52d1b6ed7d57c7f102a09b2a479d17ca42
Caen De Silva <caen@desilva.se>
2023-03-23 10:17:10 +0100

    Move text to else branch to colour output green when passed

--- tests/unit-tests.sh
+++ tests/unit-tests.sh
@@ -6,10 +6,11 @@ start=$(date +%s%3N)
 exit_code=0
 
 for test in tests/unit/*.sh; do
-  echo "Running $test"
   if ! bash "$test"; then
     echo -e "\033[0;31mTest failed:\033[0m $test";
     exit_code=1
+  else
+    echo -e "\033[0;32mTest passed:\033[0m $test";
   fi
 done
 
