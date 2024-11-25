fe0cdab4bfd3683ed8ffaf1b1a915727862ff478
Caen De Silva <caen@desilva.se>
2024-11-25 20:49:55 +0100

    Get the path dynamically

--- print-deprecation-warning.sh
+++ print-deprecation-warning.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Usage: print-deprecation-warning.sh "old_input" "new_input"
-# Example: print-deprecation-warning.sh "env-site-name" "env: SITE_NAME=value"
+# Usage: print-deprecation-warning.sh "old_input" "new_input" "workflow_file"
+# Example: print-deprecation-warning.sh "env-site-name" "env: SITE_NAME=value" ".github/workflows/build.yml"
 
 old_input=$1
 new_input=$2
+workflow_file=$3
 
-echo "::warning file=.github/workflows/build.yml,title=Deprecation Notice::The '$old_input' input is deprecated and will be removed in \`hydephp/action\` v2.0. Please use '$new_input' instead. See https://github.com/hydephp/action/pull/46 for more information."
+echo "::warning file=$workflow_file,title=Deprecation Notice::The '$old_input' input is deprecated and will be removed in \`hydephp/action\` v2.0. Please use '$new_input' instead. See https://github.com/hydephp/action/pull/46"
